08f76f178a16b428883161613970da8efb921f63
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-03-23 15:38:08 -0400

    chore(test): wrote test demo server

---
+++ test/server.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const koa = require('koa')
+const serviceVersion = require('koa-version-header')
+
+const app = koa()
+app.use(serviceVersion())
+app.use(function * () {
+  this.body = 'ok'
+})
+app.listen(3000)
+
+console.log('make any request to port 3000 to see X-... headers')
