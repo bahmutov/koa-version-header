c2ad0bf5ff685fa88ee235995cfb3ab94ba64337
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-03-23 17:49:14 -0400

    chore(demo): explaining demo code

--- demo/ping.js
+++ demo/ping.js
@@ -16,12 +16,15 @@ const server = app.listen(port)
 console.log('ping server at port', port)
 
 // fake client
+// list run time service dependencies
+// https://github.com/npm/node-semver#advanced-range-syntax
 const serviceDependencies = {
   'test-service': '~2.3.0'
 }
 const semver = require('semver')
 const axios = require('axios')
-// Note, url is often from EXTERNAL configuration
+// Note, url is often from EXTERNAL configuration, thus hard
+// to predict what server version is actually running and responds
 const url = `http://localhost:${port}`
 axios.get(url)
   .then((response) => {
@@ -29,8 +32,18 @@ axios.get(url)
     const serviceVersion = response.headers['x-service-version']
     console.log(`got response from ${serviceName}@${serviceVersion}`)
     const expectedVersion = serviceDependencies[serviceName]
-    console.log(`is this service version acceptable? We need ${expectedVersion}`)
-    const satisfies = semver.satisfies(serviceVersion, expectedVersion)
-    console.log(satisfies)
+    if (typeof expectedVersion === 'string') {
+      console.log(`is this service version acceptable? We need ${expectedVersion}`)
+      // https://github.com/npm/node-semver#ranges-1
+      const satisfies = semver.satisfies(serviceVersion, expectedVersion)
+      console.log(`satisfies? ${satisfies}`)
+    }
   })
   .then(() => server.close())
+  .catch(console.error)
+/*
+ping server at port 3000
+got response from test-service@2.3.1
+is this service version acceptable? We need ~2.3.0
+satisfies? true
+*/
